06f22f7da511b4f6524934ca911052be42948cda
Aditya Sharma <22263756+AdityaSharma6@users.noreply.github.com>
2019-08-29 13:56:04 -0400

    Removed User AAuth Error

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from flaskblog.models import *
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     # It is good practice to segment different parts of a website into different code files
