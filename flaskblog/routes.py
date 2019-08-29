7f7d43a0a16d5d2e404226b0e94ae2e1a006783c
Aditya Sharma <22263756+AdityaSharma6@users.noreply.github.com>
2019-08-29 14:42:55 -0400

    Logout Route

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect
-from flask_login import login_user
+from flask_login import login_user, current_user, logout_user
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User, Post
@@ -33,6 +33,8 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST']) # In the routes, you would use the parameter "methods" to input certain requests in LIST Format.
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode("utf-8") # Since the object in def Register() is of the Register Class. It will have its variables as objects. You specify the mini-object you want to access and then write .data to access its data
@@ -47,8 +49,10 @@ def register():
 
 @app.route("/login", methods= ["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
     form = LoginForm()
-    # Since a login form has different fields, we want to make sure that the fields are entered correctly.
+    # Since a login form has different fields, we want to make sure that the fields are entered correctly.  
     if form.validate_on_submit(): # It is essentially checking if any validation errors are raised. If validation errors that we specified in the forms.py are not raised, then it will proceed
         user = User.query.filter_by(email=form.email.data.lower()).first() # It will check if the user exists in the database by quering based off the inputted email.
         if user and bcrypt.check_password_hash(user.password, form.password.data): # If the user exists (the email is in the db) and the password they entered is the same as the password in the db. It performs its check by unhashing the value in the db and comparing it to the form data.
@@ -58,3 +62,8 @@ def login():
         else:
             flash(f"Your email or password was incorrect. Please try again!", "danger")
     return render_template("login.html", title = "Login", form=form)
+
+@app.route("/logout", methods = ["GET", "POST"])
+def logout():
+    logout_user()
+    return (redirect(url_for("home")))
