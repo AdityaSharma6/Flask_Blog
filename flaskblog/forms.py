13ecbf91c3d4741af5a4db1176c7f53b40e5e914
Aditya Sharma <22263756+AdityaSharma6@users.noreply.github.com>
2019-09-01 23:20:00 -0400

    UpdateAccountForm

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
@@ -14,8 +15,8 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField("Sign Up")
 
     def validate_username(self, username):
-        user = User.query.filter_by(username = username.data).first()
-        if user:
+        user = User.query.filter_by(username = username.data).first() # This queries the db to check if the username exists
+        if user: # If the user object != None meaning, if the user object exists. Then it means that the inputted username is taken
             raise ValidationError("That username is taken, please choose another.")
     
     def validate_email(self, email): # Email is a parameter because with back-end development, it represents the field in the object when it is created.
@@ -32,6 +33,25 @@ class LoginForm(FlaskForm):
     remember = BooleanField("Remember Me")
     password = PasswordField("Password", validators = [DataRequired()])
     login = SubmitField("Login")
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField("Username", validators = [DataRequired(), Length(min = 2)])
+    email = StringField("Email", validators = [DataRequired(), Length(min = 5), Email()])
+    submit = SubmitField("Update")
+
+    def validate_username(self, username):
+        if username.data != current_user.username: # Username is the field defined above. .data brings the raw text inputted
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError("That username is taken. Please choose another.")
+    
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError("That email is taken. Please choose another.")
+
+
 
 # Whenever we are using forms, we need to make sure that it is secure
 # We need to set a secret key -- it'll protect against modifying cookies and cross-site requests.
